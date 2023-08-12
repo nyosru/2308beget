dbfdafe9dfd10c973c227759f741a944469244b2
Sergey <1@php-cat.com>
2023-08-12 20:37:21 +0500

    старт

---
+++ app/Http/Controllers/Domain/DomainController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Domain;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DomainController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $in = [
+            'BOT_USERNAME' => 'WaitingDomainBot',
+            'BOT_TOKEN' => '6290040530:AAFKIl0csADmwkXYUCSqEWXwlbFOItlv9Hg',
+            'REDIRECT_URI' => 'https://domain.php-cat.com/api/auth/callback',
+            'HTTP_HOST' => $_SERVER['HTTP_HOST']
+        ];
+
+        return view('domain.index', $in );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/domain/index.blade.php
@@ -0,0 +1,36 @@
+@extends('domain.layouts.master')
+
+@section('content')
+
+<main class="px-3">
+
+  <script src="https://telegram.org/js/telegram-widget.js?2" data-telegram-login="{{ $BOT_USERNAME }}" data-size="medium" data-auth-url="{{$REDIRECT_URI}}" data-request-access="write"></script>
+
+    <div class="text-center" >
+    <h1>Быстрый доступ к документам</h1>    
+    
+    <p class="lead">Секретный доступный метод хранения и показа документов.</p>    
+    
+    <p class="lead">Войти в свои документы: </p>
+    
+    {{-- <p class="lead">
+      <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Узнать больше</a>
+    </p> --}}
+    
+</div>
+  </main>
+
+    {{-- <h1>Hello World</h1>
+    <p>
+        This view is loaded from module: {!! config('ttt.name') !!}
+    </p> --}}
+    {{-- <div id="app"></div> --}}
+
+    {{-- @include('ttt.header') --}}
+    {{-- @include('ttt.blockCenter') --}}
+
+    {{-- @include('ttt.backw') --}}
+
+    {{-- @include('ttt.footer') --}}
+
+@endsection
