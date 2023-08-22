56f83b4fbd7d34c6ea724fba1a01aea1e07156c7
Sergey <1@php-cat.com>
2023-08-22 10:29:59 +0500

    62

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            DomainSeeder::class,
+//            DomainSeeder::class,
             DomainPaySeeder::class,
             PromocodeSeeder::class,
             DomainPricesSeeder::class
