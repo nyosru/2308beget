6eef15158ed553958389a807bfbd5fd873e784fc
Sergey <1@php-cat.com>
2023-08-24 14:10:01 +0500

    85

--- routes/web.php
+++ routes/web.php
@@ -40,8 +40,8 @@ $d = function () {
 };
 
 //    Route::group(array('domain' => (strpos($_SERVER['HTTP_HOST'], 'dev') !== false) ? 'domain.dev.php-cat.com' : 'domain.php-cat.com' ), $d);
-    Route::group(array('domain' => 'domain.dev.php-cat.com'), $d);
-//    Route::group(array('domain' => 'domain.php-cat.com'), $d);
+//    Route::group(array('domain' => 'domain.dev.php-cat.com'), $d);
+    Route::group(array('domain' => 'domain.php-cat.com'), $d);
 
 
 //Route::get('/', function () {
