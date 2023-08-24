be09314c48a799dd7d6abfde6eabca7413df351f
Sergey <1@php-cat.com>
2023-08-24 14:09:06 +0500

    83

--- resources/views/job2308/index.blade.php
+++ resources/views/job2308/index.blade.php
@@ -4,33 +4,58 @@
     <main class="px-3">
 
 {{--        data: {{ print_R($data) }}--}}
+<style>
+    .up_tr{
+        position: sticky;
+        top: 0;
+        z-index: 11;
+        background-color: rgba(255, 255, 255, 0.9);
+    }
+    .up_tr td{ border-right: 1px solid rgb(50,50,50);}
+</style>
 
 <table class="table" >
-    <tr>
+    <tr class="up_tr">
+
+        <td>&nbsp;</td>
 
         <td>            1111        </td>
         <td>            1111        </td>
         <td>            1111        </td>
-
-        <td>            1111        </td>
-        <td>            1111        </td>
-        <td>            1111        </td>
-
-        <td>            1111        </td>
         <td>            1111        </td>
         <td>            1111        </td>
 
         <td>            1111        </td>
         <td>            1111        </td>
+        <td>            1111        </td>
+        <td>            1111        </td>
+        <td>            1111        </td>
+
+        <td>            1111        </td>
+        <td>            1111        </td>
+        <td>            1111        </td>
+        <td>            1111        </td>
+        <td>            1111        </td>
+
+        <td>            1111        </td>
+        <td>            1111        </td>
+        <td>            1111        </td>
+        <td>            1111        </td>
+        <td>            1111        </td>
+
         <td>            1111        </td>
 
     </tr>
     @foreach( $data as $row )
     <tr>
-        <td class="p-0">{{ $names[rand(1,sizeof($names)-1)] }}</td>
+        <td class="p-0"
+            style="position: sticky; left: 0;
+            z-index: 10;
+            background-color: rgba(255,255,255,0.9);"
+        >{{ $names[rand(1,sizeof($names)-1)] }}</td>
         @foreach( $row as $col )
 {{--            <td>col: {{ print_r($col) }}</td>--}}
-            <td class="p-1">
+            <td class="p-1" style="z-index: 9;">
 {{--                @if( $col['type'] == 1 )--}}
 {{--                    11 {{ $col['val'] }}--}}
                     @include('job2308.in2norm', $col )
