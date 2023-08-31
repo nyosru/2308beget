38bd6aa1768874a96f44aa46f7e8906119824f6f
Sergey <1@php-cat.com>
2023-08-31 23:27:01 +0500

    202

--- resources/views/domain/layouts/footer.blade.php
+++ resources/views/domain/layouts/footer.blade.php
@@ -1,7 +1,14 @@
 <footer class="mt-auto text-white-50 text-center" style="background-color:rgba(0,0,0,0.2);">
-  {{-- <p>Шаблон обложки для <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, от <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p> --}}
-  <p class="p-3">
-    &copy; 2020-{{ date('Y') }}
-  </p>
-  <a href="https://php-cat.com" style="display: block; position:fixed; right:10px; bottom: 10px;">Создание сайта php-cat.com</a>
+    {{-- <p>Шаблон обложки для <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, от <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p> --}}
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6"><p class="p-3">&copy; 2020-{{ date('Y') }} </p></div>
+            <div class="col-12 col-md-6"><p class="p-3"><A href="{{ route('domain_backword') }}">Обратная связь</A></p>
+            </div>
+        </div>
+    </div>
+
+
 </footer>
+<a href="https://php-cat.com" style="display: block; position:fixed; right:10px; bottom: 10px;">Создание сайта
+    php-cat.com</a>
