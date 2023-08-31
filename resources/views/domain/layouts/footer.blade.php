d44356843f3bd7355ab579f7955329992773bfce
Sergey <1@php-cat.com>
2023-08-31 23:31:55 +0500

    206

--- resources/views/domain/layouts/footer.blade.php
+++ resources/views/domain/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="mt-auto text-white-50 text-center" style="background-color:rgba(0,0,0,0.2);">
+<footer class="mt-auto pt-5 text-white-50 text-center" style="background-color:rgba(0,0,0,0.2);">
     {{-- <p>Шаблон обложки для <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, от <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p> --}}
     <div class="container">
         <div class="row">
@@ -12,5 +12,5 @@
     </div>
 
 </footer>
-<a href="https://php-cat.com" style="display: block; position:fixed; right:10px; bottom: 10px;">Создание сайта
+<a href="https://php-cat.com" style="display: block; position:fixed; right:10px; bottom: 40px;">Создание сайта
     php-cat.com</a>
