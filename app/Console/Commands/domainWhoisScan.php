2043954fa260e4bafb4cbf92690befda743d2f8d
Sergey <1@php-cat.com>
2023-09-01 12:28:03 +0500

    208

--- app/Console/Commands/domainWhoisScan.php
+++ app/Console/Commands/domainWhoisScan.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Http\Controllers\Domain\WhoisController;
+use App\Http\Controllers\TelegramController;
 use Illuminate\Console\Command;
 
 class domainWhoisScan extends Command
@@ -24,18 +25,22 @@ class domainWhoisScan extends Command
     /**
      * Execute the console command.
      */
-    public function handle(WhoisController $whc)
+    public function handle(WhoisController $whc, TelegramController $tele)
     {
 
         $this->info('Сканирование whois информации о доменах');
 
         try {
             $e = $whc->whoisUpdate(10, 'array');
+            $msg = 'скан доменов ';
             foreach ($e as $domain) {
                 $this->success($domain);
+                $msg .= ' '.$domain;
             }
+            $tele::send($msg);
         } catch (\Exception $ex) {
             $this->error('нет доменов для сканироваиня');
+            $tele::send('нет доменов для сканироваиня');
         }
 
     }
