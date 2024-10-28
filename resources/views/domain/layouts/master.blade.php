a3ac9dfde19aa94794e6d85d3727e442f7f41cc4
Sergey <1@php-cat.com>
2024-10-28 06:11:11 +0500

    03-01

--- resources/views/domain/layouts/master.blade.php
+++ resources/views/domain/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>СкладДокументов</title>
+    <title>DomainWaiter.com</title>
 
     {{-- Laravel Mix - CSS File --}}
     {{-- <link rel="stylesheet" href="{{ mix('css/ttt.css') }}"> --}}
